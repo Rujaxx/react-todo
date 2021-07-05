e58d8791a1e6045f7e8e505ec35edad2bf0f2858
Rujaxx <shaikhrujan@gmail.com>
2021-07-05 10:14:10 +0530

    add button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
+import { v4 as uuidv4 } from 'uuid';
 import TodoList from "./TodoList";
 function App() {
-  const [todos,setTodos] = useState(["todo1","todo2"]) 
+  const [todos,setTodos] = useState([])
+  function handleAdd (e) {
+    const name = nameRef.current.value
+    if(name === ""){
+      return alert("enter task please")
+    }
+    else{
+      setTodos (prevTodos => {
+       return [...prevTodos,{id: uuidv4(), name:name , complete : false }]
+      })
+    }
+  }
+  const nameRef = useRef(); 
   return (
     <>
     <TodoList todos = {todos}/>
-    <input type = "text"/>
-    <button>Add Todo</button>
+    <input ref = {nameRef} type = "text"/>
+    <button onClick= {handleAdd}>Add Todo</button>
     <button>Clear Completed</button>
     <div>0 Left to do</div>
     </>
--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Todo({todo}) {
     return (
         <div>
-            {todo}
+            <label>
+                <input type = "checkbox" checked = {todo.complete}/>
+                {todo.name}
+            
+            </label>
+            
         </div>
     )
 }
